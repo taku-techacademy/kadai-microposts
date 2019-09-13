960b3f3da13b265d00935623d3d69100015ea458
taku.yahagi <scandal.sg@gmail.com>
2019-09-13 13:33:43 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_loggged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show]
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order(id: :desc).page(params[:page])
+    counts(@user)
   end
 
   def new
